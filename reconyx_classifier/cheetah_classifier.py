6fc7cfc8a2c846c3e4dbc531470ee15dcea4582a
Benjamin Strauch <b.strauch@fu-berlin.de>
2018-03-12 22:56:54 +0100

    Simple classifier skeleton.

--- reconyx_classifier/cheetah_classifier.py
+++ reconyx_classifier/cheetah_classifier.py
@@ -1,13 +1,61 @@
 import sys
-from data_utils.classifier import ImageClassifier
+import argparse
+
+import logging
+
+
+
+def parse_arguments():
+    """ Parse the sys.argv command line arguments.
+    :return: A NameSpace object with the parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description="Reconyx image classifier")
+    optional = parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+
+    parser.add_argument(dest='directory',
+                        help="Images to be classified.")
+
+    required.add_argument('--model', required=True,
+                          help="Stored Keras model to load for classification.")
+
+    optional.add_argument('--copy_output', action='store_true',
+                          help="Copy classified images to output directories.")
+
+    optional.add_argument('-v', '--verbose', help="Increase output verbosity.",
+                          action='store_const', const=logging.DEBUG,
+                          default=logging.INFO)
+
+    parser._action_groups.append(optional)
+
+    args = parser.parse_args()
+    return args
 
 
 def main():
-    args = sys.argv[1:]
-    print("Hello %s", args)
+    args = parse_arguments()
 
-    im_class = ImageClassifier(args[0])
-    input("Press any key to continue.")
+    log = logging.getLogger(__name__)
+    logging.basicConfig(stream=sys.stdout, level=args.verbose)
+
+    # import only after parsing to reduce startup delay
+    from data_utils.classifier import ImageClassifier
+    from data_utils.reader import read_dir_metadata
+
+    log.info("Loading model from '{}'".format(args.model))
+    try:
+        # im_class = ImageClassifier(args.model)
+        log.info("Model successfully loaded")
+    except OSError as err:
+        log.error("OS error: {}".format(err))
+        sys.exit(1)
+
+    # im_class.classify_directory(args.directory)
+    log.info("Classifying images in '{}'".format(args.directory))
+    # read_dir_metadata(args.directory)
+    data = read_dir_metadata(args.directory)
+    print(data.head(10))
+    # input("Press any key to continue.")
 
 
 if __name__ == '__main__':
--- reconyx_classifier/data_utils/classifier.py
+++ reconyx_classifier/data_utils/classifier.py
@@ -1,8 +1,11 @@
+import os
 import pandas as pd
 import keras.models
 
+# import logging
+# log = logging.getLogger(__name__)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
-# from keras.models import load_model
 
 class ImageClassifier:
     """The image classifier for the Reconxy images.
@@ -19,4 +22,35 @@ class ImageClassifier:
             The model should output a softmax activation of the classes.
         """
 
+        # log.info("Initializing ImageClassifier")
         self.model = keras.models.load_model(model_path)
+
+    def classify_data(self, data):
+        """ Classify the data described by a Pandas dataframe.
+
+        :param data: pandas.DataFrame
+            A data frame with columns containing at least the following:
+            file path - path to the image file
+            simple_event_key - the event the image belongs to
+            event_key - the extended event when 3-image events are merged
+
+        :returns: pandas.DataFrame
+            The data frame, with a new 'label' column.
+        """
+
+        return data
+
+    def classify_directory(self, dir_path):
+        """ Classify all image under this directory.
+
+        Classifies images in this directory and subdirectories.
+
+        :param dir_path: string
+            Path to image root directory.
+        :return: pandas.DataFrame
+            Data frame with some image metadata, labels and image paths.
+        """
+
+        metadata = pd.DataFrame()
+
+        return metadata
