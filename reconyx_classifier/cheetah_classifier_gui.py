5a1a848fc5d9715991313e548315a671a6b5eac3
Benjamin Strauch <b.strauch@fu-berlin.de>
2018-04-13 23:57:25 +0200

    Refactored threading into single worker thread.
    The workflow is now more intuitive and tasks can be removed.

--- reconyx_classifier/cheetah_classifier_gui.py
+++ reconyx_classifier/cheetah_classifier_gui.py
@@ -5,6 +5,8 @@ from PyQt5.QtGui import *
 import design
 import sys
 
+from queue import Queue
+
 # classification imports
 from data_utils import *
 import pandas as pd
@@ -17,54 +19,79 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format="%(levelname)-7s - %(name)-10s - %(message)s")
 
 
-class WorkerSignals(QObject):
+class DataInformation:
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.data = None
+
+
+class ReadWorker(QObject):
     """Slot class for notifying main thread about worker progress."""
 
     finished = pyqtSignal()
     progress = pyqtSignal(int)
-    error = pyqtSignal(str)
-    result = pyqtSignal(tuple)
+    error = pyqtSignal(object)
+    result = pyqtSignal(object)
 
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-class ReaderWorker(QRunnable):
-    """Worker responsible for scanning the image metadata."""
+        self.task_queue = Queue()
 
-    def __init__(self, dirpath):
-        super(ReaderWorker, self).__init__()
-        self.dirpath = dirpath
-        self.signals = WorkerSignals()
+    def process_tasks(self):
+        while not self.task_queue.empty():
+            item = self.task_queue.get()
+            # extract directory path out of the item
+            dirpath = item.data_path
 
-        self.runnable = True;
+            if dirpath == "":
+                print("Directory was removed")
+                continue
 
-    def run(self):
-        if not self.runnable:
-            self.signals.error.emit(self.dirpath)
-            return
+            try:
+                data = read_dir_metadata(dirpath,
+                                         progress_callback=self.report_progress)
+                item.data = data
+                self.result.emit(item)
+            except FileNotFoundError:
+                self.error.emit(item)
+            finally:
+                self.finished.emit()
 
-        try:
-            data = read_dir_metadata(self.dirpath,
-                                     progress_callback=self.report_progress)
-            self.signals.result.emit((self.dirpath, data))
-        except FileNotFoundError:
-            self.signals.error.emit(self.dirpath)
-        finally:
-            self.signals.finished.emit()
+    @pyqtSlot(object)
+    def read_directory(self, item):
+        print("Received path {}".format(str(item.data_path)))
+
+        self.task_queue.put(item)
+        self.process_tasks()
+
+    def stop_processing(self):
+        pass
 
     def report_progress(self, val):
-        self.signals.progress.emit(val)
+        self.progress.emit(val)
 
 
 class ExampleApp(QMainWindow, design.Ui_MainWindow):
+    read_signal = pyqtSignal(object)
+
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
 
         # internal data
         self.image_data = {}
-        self.thread_pool = QThreadPool()
-        self.thread_pool.setMaxThreadCount(1)
+        self.read_thread = QThread(self)
 
-        # self.reader_thread = ReaderWorker()
+        self.read_worker = ReadWorker()
+        self.read_worker.result.connect(self.update_success)
+        self.read_worker.error.connect(self.update_error)
+        self.read_worker.progress.connect(self.update_progress)
+        self.read_worker.finished.connect(self.finish_reader)
+        self.read_signal.connect(self.read_worker.read_directory)
+
+        self.read_worker.moveToThread(self.read_thread)
+        self.read_thread.start()
 
         # Models
         # the input directory model holds the selected input directories
@@ -79,23 +106,24 @@ class ExampleApp(QMainWindow, design.Ui_MainWindow):
         self.addDirButton.clicked.connect(self.add_input_dir)
         self.removeDirButton.clicked.connect(self.remove_input_dir)
 
+        self.stopButton.hide()
         self.progressBar.hide()
         self.label.hide()
 
-    def update_success(self, args):
-        item_path, data = args
-        self.image_data[item_path] = data
-        item = self.inputDirsModel.findItems(item_path)[0]
+    def update_success(self, data):
+        # self.image_data[data.data_path].data = data
+        item = self.inputDirsModel.findItems(data.data_path)[0]
         item.setBackground(QColor(173, 255, 47, 50))
 
-    def update_error(self, item_path):
-        item = self.inputDirsModel.findItems(item_path)[0]
+    def update_error(self, data):
+        item = self.inputDirsModel.findItems(data.data_path)[0]
         item.setBackground(QColor(255, 0, 0, 50))
 
     def update_progress(self, percent):
         self.progressBar.setValue(percent)
 
     def finish_reader(self):
+        self.stopButton.hide()
         self.progressBar.setValue(100)
 
     def add_input_dir(self):
@@ -103,33 +131,31 @@ class ExampleApp(QMainWindow, design.Ui_MainWindow):
         input_dir = QFileDialog.getExistingDirectory(
             caption="Select input directory.")
 
+        data = DataInformation(input_dir)
+        self.image_data[data.data_path] = data
+
         # QApplication.processEvents()
 
         # if no input was selected, skip
-        if input_dir == '':
+        if data.data_path == '':
             return
 
         # don't add duplicate directories
         # (only matches by path name, does not recognize symlinks etc.)
-        if len(self.inputDirsModel.findItems(input_dir)) > 0:
+        if len(self.inputDirsModel.findItems(data.data_path)) > 0:
             return
 
         # add path to file list model
         # at this point, the path is still not processed
-        item = QStandardItem(input_dir)
+        item = QStandardItem(data.data_path)
         item.setBackground(QColor(255, 250, 205, 50))
         self.inputDirsModel.appendRow(item)
 
-        # add the path to a queue for a worker thread
-        reader = ReaderWorker(input_dir)
-        reader.signals.result.connect(self.update_success)
-        reader.signals.error.connect(self.update_error)
-        reader.signals.progress.connect(self.update_progress)
-        reader.signals.finished.connect(self.finish_reader)
+        self.read_signal.emit(data)
 
+        self.stopButton.show()
         self.progressBar.show()
         self.label.show()
-        self.thread_pool.start(reader)
 
     def remove_input_dir(self):
         removed_items = []
@@ -141,6 +167,8 @@ class ExampleApp(QMainWindow, design.Ui_MainWindow):
 
         # remove the selected items from the input directory model
         for item in removed_items:
+            self.image_data[item.text()].data_path = ''
+            del self.image_data[item.text()]
             self.inputDirsModel.removeRow(item.row())
 
 
