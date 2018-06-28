b940adde82a8e4d9dda9e60a3c490e2b624e4e98
Benjamin Strauch <b.strauch@fu-berlin.de>
2018-06-28 20:53:32 +0200

    Also log messages to a log file.

--- reconyx_classifier/cheetah_classifier_gui.py
+++ reconyx_classifier/cheetah_classifier_gui.py
@@ -3,13 +3,16 @@ from PyQt5.QtWidgets import QApplication
 from gui_view import ClassificationApp
 import sys
 
-
 import logging
 
 log = logging.getLogger(__name__)
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-                    format="%(levelname)-7s - %(name)-10s - %(message)s")
+logging.basicConfig(level=logging.INFO,
+                    handlers=[logging.FileHandler("classifier.log", mode="w"),
+                              logging.StreamHandler(stream=sys.stdout)],
+                    format="%(asctime)s - %(levelname)-7s"
+                           " - %(name)-10s - %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
 
 
 def main():
